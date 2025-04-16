9def58b3df39badf0cf5975ece953003620f6ff2
AleksandrinaAA <55195384+AleksandrinaAA@users.noreply.github.com>
2025-04-16 19:58:05 +0300

    Merge pull request #2
    * added base post endpoints
    
    * removed excess

--- src/main/java/yandex/infrastructure/PostController.java
+++ src/main/java/yandex/infrastructure/PostController.java
@@ -2,19 +2,38 @@ package yandex.infrastructure;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
 public class PostController {
 
-    @GetMapping("/posts/add")
-    private String addPost() {
-        return "add-post";
+    @GetMapping("/")
+    private String home() {
+        return "redirect:/posts";
     }
 
     @GetMapping("/posts")
     private String getPosts() {
         return "posts";
     }
+
+    @GetMapping("/posts/add")
+    private String addPost() {
+        return "add-post";
+    }
+
+    @PostMapping("/posts")
+    private String savePost() {
+        return "posts";
+    }
+
+    @PostMapping("/posts/{id}")
+    private String editPost(@PathVariable(required = false) String id) {
+        return "posts";
+    }
+
+
 }
